5b337a4680abe8be61c42d692996d72713180e12
stayseek <stayseek0@gmail.com>
2020-02-14 12:36:32 +0300

    HW2_done_but
    Database still not created

--- Assets/Scripts/Controllers/MyNetworkManager.cs
+++ Assets/Scripts/Controllers/MyNetworkManager.cs
@@ -1,101 +1,161 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
+using System;
+using System.Collections;
+using UnityEngine.Networking.NetworkSystem;
+using UnityEngine.SceneManagement;
+using DatabaseControl;
 
 public class MyNetworkManager : NetworkManager
 {
+    public Action<string> loginResponse;
+    public Action<string> registerResponse;
+
     public bool ServerMode;
     void Start()
     {
         if (ServerMode)
         {
             StartServer();
+            NetworkServer.UnregisterHandler(MsgType.Connect);
+            NetworkServer.RegisterHandler(MsgType.Connect, OnServerConnectCustom);
+            NetworkServer.RegisterHandler(MsgType.Highest + (short)NetMsgType.Login, OnServerLogin);
+            NetworkServer.RegisterHandler(MsgType.Highest + (short)NetMsgType.Register, OnServerRegister);
         }
+
     }
-    public override void OnStartServer()
+    void OnServerLogin(NetworkMessage netMsg)
     {
-        Debug.Log("Server has started");
+        StartCoroutine(LoginUser(netMsg));
     }
 
-    public override void OnStopServer()
+    void OnServerRegister(NetworkMessage netMsg)
     {
-        Debug.Log("Server has stopped");
-    }
-    public override void OnStartHost()
-    {
-        Debug.Log("Host has started");
-    }
-    public override void OnStopHost()
-    {
-        Debug.Log("Host has stopped");
-    }
-    public override void OnServerConnect(NetworkConnection conn)
-    {
-        Debug.Log("A client connected to the server: " + conn);
+        StartCoroutine(RegisterUser(netMsg));
     }
 
-    public override void OnServerDisconnect(NetworkConnection conn)
+    void OnClientLogin(NetworkMessage netMsg)
     {
-        NetworkServer.DestroyPlayersForConnection(conn);
-        if (conn.lastError != NetworkError.Ok)
+        loginResponse.Invoke(netMsg.reader.ReadString());
+    }
+
+    void OnClientRegister(NetworkMessage netMsg)
+    {
+        registerResponse.Invoke(netMsg.reader.ReadString());
+    }
+
+    void OnServerConnectCustom(NetworkMessage netMsg)
+    {
+        if (LogFilter.logDebug) 
+        { 
+            Debug.Log("NetworkManager:OnServerConnectCustom"); 
+        }
+        netMsg.conn.SetMaxDelay(maxDelay);
+        OnServerConnect(netMsg.conn);
+    }
+
+    public void Login(string login, string pass)
+    {
+        ClientConnect();
+        StartCoroutine(SendLogin(login, pass));
+    }
+
+    public void Register(string login, string pass)
+    {
+        ClientConnect();
+        StartCoroutine(SendRegister(login, pass));
+    }
+    IEnumerator SendLogin(string login, string pass)
+    {
+        while (!client.isConnected)
         {
-            if (LogFilter.logError) { Debug.LogError("ServerDisconnected due to error: " + conn.lastError); }
+            yield return null;
         }
-        Debug.Log("A client disconnected from the server: " + conn);
+        Debug.Log("client login");
+        client.connection.Send(MsgType.Highest + (short)NetMsgType.Login, new UserMessage(login, pass));
     }
 
-    public override void OnServerReady(NetworkConnection conn)
+    IEnumerator SendRegister(string login, string pass)
     {
-        NetworkServer.SetClientReady(conn);
-        Debug.Log("Client is set to the ready state (ready to receive state updates): " + conn);
-    }
-    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
-    {
-        GameObject player = Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
-        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
-        Debug.Log("Client has requested to get his player added to the game");
-    }
-    public override void OnServerRemovePlayer(NetworkConnection conn, UnityEngine.Networking.PlayerController player)
-    {
-        if (player.gameObject != null)
-            NetworkServer.Destroy(player.gameObject);
-    }
-    public override void OnServerError(NetworkConnection conn, int errorCode)
-    {
-        Debug.Log("Server network error occurred: " + (NetworkError)errorCode);
-    }
-    public override void OnStartClient(NetworkClient client)
-    {
-        Debug.Log("Client has started");
-    }
-    public override void OnStopClient()
-    {
-        Debug.Log("Client has stopped");
-    }
-    public override void OnClientConnect(NetworkConnection conn)
-    {
-        base.OnClientConnect(conn);
-        Debug.Log("Connected successfully to server, now to set up other stuff for the client...");
-    }
-    public override void OnClientDisconnect(NetworkConnection conn)
-    {
-        StopClient();
-        if (conn.lastError != NetworkError.Ok)
+        while (!client.isConnected)
         {
-            if (LogFilter.logError) { Debug.LogError("ClientDisconnected due to error: " + conn.lastError); }
+            yield return null;
         }
-        Debug.Log("Client disconnected from server: " + conn);
+        Debug.Log("client register");
+        client.connection.Send(MsgType.Highest + (short)NetMsgType.Register, new UserMessage(login, pass));
     }
-    public override void OnClientNotReady(NetworkConnection conn)
+    IEnumerator LoginUser(NetworkMessage netMsg)
     {
-        Debug.Log("Server has set client to be not-ready (stop getting state updates)");
+        UserMessage msg = netMsg.ReadMessage<UserMessage>();
+        IEnumerator e = DCF.Login(msg.login, msg.pass);
+
+        while (e.MoveNext())
+        {
+            yield return e.Current;
+        }
+        string response = e.Current as string;
+
+        if (response == "Success")
+        {
+            Debug.Log("server login success");
+            netMsg.conn.Send(MsgType.Scene, new StringMessage(SceneManager.GetActiveScene().name));
+        }
+        else
+        {
+            Debug.Log("server login fail");
+            netMsg.conn.Send(MsgType.Highest + (short)NetMsgType.Login, new StringMessage(response));
+        }
     }
-    public override void OnClientError(NetworkConnection conn, int errorCode)
+
+    IEnumerator RegisterUser(NetworkMessage netMsg)
     {
-        Debug.Log("Client network error occurred: " + (NetworkError)errorCode);
+        UserMessage msg = netMsg.ReadMessage<UserMessage>();
+        IEnumerator e = DCF.RegisterUser(msg.login, msg.pass, "");
+
+        while (e.MoveNext())
+        {
+            yield return e.Current;
+        }
+        string response = e.Current as string;
+
+        Debug.Log("server register done");
+        netMsg.conn.Send(MsgType.Highest + (short)NetMsgType.Register, new StringMessage(response));
     }
-    public override void OnClientSceneChanged(NetworkConnection conn)
+    void ClientConnect()
     {
-        base.OnClientSceneChanged(conn);
-        Debug.Log("Server triggered scene change and we've done the same, do any extra work here for the client...");
+        NetworkClient client = this.client;
+        if (client == null)
+        {
+            client = StartClient();
+            client.RegisterHandler(MsgType.Highest + (short)NetMsgType.Login, OnClientLogin);
+            client.RegisterHandler(MsgType.Highest + (short)NetMsgType.Register, OnClientRegister);
+        }
+    }
+}
+public class UserMessage : MessageBase
+{
+    public string login;
+    public string pass;
+
+    public UserMessage()
+    {
+    }
+
+    public UserMessage(string login, string pass)
+    {
+        this.login = login;
+        this.pass = pass;
+    }
+
+    public override void Deserialize(NetworkReader reader)
+    {
+        login = reader.ReadString();
+        pass = reader.ReadString();
+    }
+
+    public override void Serialize(NetworkWriter writer)
+    {
+        writer.Write(login);
+        writer.Write(pass);
     }
 }
---
+++ Assets/Scripts/Enums/NetMsgType.cs
@@ -0,0 +1,5 @@
+﻿public enum NetMsgType 
+{ 
+    Login,
+    Register
+}
